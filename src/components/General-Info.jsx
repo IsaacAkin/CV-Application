70f2ecfc61cd07a68b1e7ed24405c24ba12c1bef
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-01-22 05:20:35 +0000

    feat: added state to the general info component

--- src/components/General-Info.jsx
+++ src/components/General-Info.jsx
@@ -1,19 +1,41 @@
 import '../styles/utils.css';
+import { useState } from 'react';
 import { Input, Button } from "./utils.jsx";
 
 export default function GeneralInfo() {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState(null);
+
+    function handleFirstName(e) {
+        return setFirstName(e.target.value);
+    }
+
+    function handleLastName(e) {
+        return setLastName(e.target.value);
+    }
+
+    function handleEmail(e) {
+        return setEmail(e.target.value);
+    }
+
+    function handlePhoneNumber(e) {
+        return setPhoneNumber(e.target.value);
+    }
 
     return (
         <>
             <h2>General Information</h2>
             <section className='general-info'>
                 <div className="part-one">
-                    <Input labelName={'First Name'} inputType={'text'} />
-                    <Input labelName={'Last Name'} inputType={'text'} />
+                    <Input labelName={'First Name'} inputType={'text'} value={firstName} onChange={handleFirstName} />
+                    <Input labelName={'Last Name'} inputType={'text'} value={lastName} onChange={handleLastName} />
                 </div>
                 <div className="part-two">
-                    <Input labelName={'Date of Birth'} inputType={'date'}/>
-                    <Input labelName={'Phone Number'} inputType={'tel'}/>
+                    <Input labelName={'Email'} inputType={'email'} value={email} onChange={handleEmail} />
+                    <Input labelName={'Phone Number'} inputType={'tel'} value={phoneNumber} onChange={handlePhoneNumber} />
                 </div>
                 <div className="buttons-container">
                     <Button name={'edit'} />
