4cf8d0d9c24461b93ace1a24571ad56ddab627a5
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-04-20 21:03:10 +0100

    Removed state from GeneralInfo and Education components
    Removed state from the GeneralInfo component in favour of
    adding state to the parent component so that text values
    can be shown when typing. The parent component as of this
    time will be App.
    
    Removed the value prop from the Input component in line with
    the previous commit.
    
    Added props for onChange functions to allow state to be
    updated.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,19 +1,18 @@
 import '../styles/utils.css';
 import { Input, Button } from "./utils.jsx";
 
-export default function Education() {
-
+export default function Education({ handleSchoolName, handleDegreeTitle, handleStartOfStudy, handleEndOfStudy}) {
     return (
         <>
             <h2>Education</h2>
             <section className='education'>
                 <div className="part-one">
-                    <Input labelName={'School Name'} inputType={'text'} />
-                    <Input labelName={'Title of Study'} inputType={'text'} />
+                    <Input labelName={'School Name'} inputType={'text'} onChange={handleSchoolName} />
+                    <Input labelName={'Degree Title'} inputType={'text'} onChange={handleDegreeTitle} />
                 </div>
                 <div className="part-two">
-                    <Input labelName={'Study from'} inputType={'date'} />
-                    <Input labelName={'Study to'} inputType={'date'} />
+                    <Input labelName={'Study from'} inputType={'date'} onChange={handleStartOfStudy} />
+                    <Input labelName={'Study to'} inputType={'date'} onChange={handleEndOfStudy} />
                 </div>
                 <div className="buttons-container">
                     <Button name={'Add Section'} />
--- src/components/General-Info.jsx
+++ src/components/General-Info.jsx
@@ -1,41 +1,18 @@
 import '../styles/utils.css';
-import { useState } from 'react';
 import { Input, Button } from "./utils.jsx";
 
-export default function GeneralInfo() {
-
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState(null);
-
-    function handleFirstName(e) {
-        return setFirstName(e.target.value);
-    }
-
-    function handleLastName(e) {
-        return setLastName(e.target.value);
-    }
-
-    function handleEmail(e) {
-        return setEmail(e.target.value);
-    }
-
-    function handlePhoneNumber(e) {
-        return setPhoneNumber(e.target.value);
-    }
-
+export default function GeneralInfo({ handleFirstName, handleLastName, handleEmail, handlePhoneNumber}) {
     return (
         <>
             <h2>General Information</h2>
             <section className='general-info'>
                 <div className="part-one">
-                    <Input labelName={'First Name'} inputType={'text'} value={firstName} onChange={handleFirstName} />
-                    <Input labelName={'Last Name'} inputType={'text'} value={lastName} onChange={handleLastName} />
+                    <Input labelName={'First Name'} inputType={'text'} onChange={handleFirstName} />
+                    <Input labelName={'Last Name'} inputType={'text'} onChange={handleLastName} />
                 </div>
                 <div className="part-two">
-                    <Input labelName={'Email'} inputType={'email'} value={email} onChange={handleEmail} />
-                    <Input labelName={'Phone Number'} inputType={'tel'} value={phoneNumber} onChange={handlePhoneNumber} />
+                    <Input labelName={'Email'} inputType={'email'} onChange={handleEmail} />
+                    <Input labelName={'Phone Number'} inputType={'tel'} onChange={handlePhoneNumber} />
                 </div>
                 <div className="buttons-container">
                     <Button name={'edit'} />
