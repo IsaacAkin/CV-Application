0129079041b9017eec70ebeb8a072393337fc0eb
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-04-21 23:35:33 +0100

    Updated Experience component
    removed values from the Input components inline with commit `34c7cb9`
    added props for handling input values

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,22 +1,21 @@
 import '../styles/utils.css';
 import { Input, Button } from "./utils.jsx";
 
-export default function Experience() {
-
+export default function Experience({ handleCompanyName, handlePositionTitle, handleEmploymentStartDate, handleEmploymentEndDate, handleResponsibilities }) {
     return (
         <>
             <h2>Experience</h2>
             <section className='experience'>
                 <div className="part-one">
-                    <Input labelName={'Company Name'} inputType={'text'} />
-                    <Input labelName={'Position Title'} inputType={'text'} />
+                    <Input labelName={'Company Name'} inputType={'text'} onChange={handleCompanyName} />
+                    <Input labelName={'Position Title'} inputType={'text'} onChange={handlePositionTitle} />
                 </div>
                 <div className="part-two">
-                    <Input labelName={'Employed from'} inputType={'date'} />
-                    <Input labelName={'Employed to'} inputType={'date'} />
+                    <Input labelName={'Employed from'} inputType={'date'} onChange={handleEmploymentStartDate} />
+                    <Input labelName={'Employed to'} inputType={'date'} onChange={handleEmploymentEndDate} />
                 </div>
                 <div className="part-three">
-                    <Input labelName={'Main Responsibilities'} inputType={'text'}/>
+                    <Input labelName={'Main Responsibilities'} inputType={'text'} onChange={handleResponsibilities} />
                     <Button name={'Add Point'} />
                 </div>
                 <div className="buttons-container">
