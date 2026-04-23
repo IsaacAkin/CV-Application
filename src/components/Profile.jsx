268f5b53d067b14b9d611d85cd2211a7f6edcd28
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-04-23 20:53:48 +0100

    Added state to the Profile component
    state is used to update the values of the textarea element

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,15 @@
-export default function Profile({ description }) {
+import { useState } from "react";
+
+export default function Profile() {
+    const [description, setDescription] = useState('');
+
+    function handleDescription(e) {
+        setDescription(e.target.value);
+    }
+
     return (
         <div>
-            <textarea name="description" id="description" cols={110}>
+            <textarea name="description" id="description" cols={105} onChange={handleDescription} placeholder='Enter a short summary about who you are and what you can bring to the table'>
                 {description}
             </textarea>
         </div>
