7d3e818efc11b3896a28d4e6c5a860d1fb61a1c5
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-04-23 17:49:54 +0100

    Re-worked the General-Info component

--- src/components/General-Info.jsx
+++ src/components/General-Info.jsx
@@ -1,34 +1,20 @@
 import '../styles/utils.css';
-import { useState } from "react";
-import { Input, Button } from "./utils.jsx";
+// import { useState } from "react";
+// import { Input, Button } from "./utils.jsx";
 
-export default function GeneralInfo({ handleFirstName, handleLastName, handleEmail, handlePhoneNumber}) {
-    const [disabled, setDisabled] = useState(false);
-    
-    const handleDisabled = () => {
-        if (!disabled) {
-            setDisabled(true);
-        } else {
-            setDisabled(false);
-        }
-    }
-
+export default function GeneralInfo({ fullName, email, phoneNumber }) {
     return (
         <>
-            <h2>General Information</h2>
-            <section className='general-info'>
-                <div className="part-one">
-                    <Input labelName={'First Name'} inputType={'text'} onChange={handleFirstName} isDisabled={disabled} />
-                    <Input labelName={'Last Name'} inputType={'text'} onChange={handleLastName} isDisabled={disabled} />
+            <h1>{fullName}</h1>
+            <div>
+                <div>
+                    <input type="text" name="" id="" value={email} />
                 </div>
-                <div className="part-two">
-                    <Input labelName={'Email'} inputType={'email'} onChange={handleEmail} isDisabled={disabled} />
-                    <Input labelName={'Phone Number'} inputType={'tel'} onChange={handlePhoneNumber} isDisabled={disabled} />
+                <span> | </span>
+                <div>
+                    <input type="number" name="" id="" value={phoneNumber} />
                 </div>
-                <div className="buttons-container">
-                    <Button name={disabled ? 'Edit' : 'Submit'} onClick={handleDisabled} />
-                </div>
-            </section>
+            </div>
         </>
     )
 }
