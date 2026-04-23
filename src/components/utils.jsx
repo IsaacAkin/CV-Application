8080ac46594927067a2812ae0579edcfe5baa3aa
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-04-23 21:10:58 +0100

    Created Input and Button components
    Input components are written to change between disabled and enabled
    depending on whether isDisabled is true or not. The Button components
    text changes based on the same logic

--- src/components/utils.jsx
+++ src/components/utils.jsx
@@ -1,22 +1,33 @@
-export function ListItem({ item }) {
+export function Input({ type, value, onChange, placeholder, isDisabled }) {
     return(
-        // <li>{item}</li>
-        <input type="text" value={item} />
+        <>
+            {
+                isDisabled
+                ? <input type={type} name="" id="" value={value} onChange={onChange} placeholder={placeholder} disabled />
+                : <input type={type} name="" id="" value={value} onChange={onChange} placeholder={placeholder} />
+            }
+        </>
     )
 }
 
-// export function Achievements({ list }) {
-//     return(
-//         <ul className='achievements'>
-//             {
-//                 list.map((item) => {
-//                     <li key={item}>{item}</li>
-//                 })
-//             }
-//         </ul>
-//     )
-// }
-// OR:
+export function Button({ onClick, disabled }) {
+    return(
+        <>
+            {
+                disabled
+                ? <button onClick={onClick}>Enable</button>
+                : <button onClick={onClick}>Disable</button>
+            }
+        </>
+    )
+}
+
+export function ListItem({ value, onChange }) {
+    return(
+        <input type="text" value={value} onChange={(e) => onChange(e.target.value)} />
+    )
+}
+
 export function Achievements({ list }) {
     return(
         <ul className='achievements'>
